ec9978131118ff2de4524429278a14ca6b584567
Pankaj <pankaj@centennialinfotech.com>
2026-05-15 19:59:50 +0530

    path

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ dotenv.config();
 import express from "express";
 import sql from "mssql";
 import cors from "cors";
-
+import path from "path";
 const app = express();
+// serve static files
+app.use(express.static("public"));
 app.use(cors());
 app.use(express.json());
 
@@ -64,17 +66,16 @@ app.get("/search", async (req, res) => {
         ORDER BY edate DESC
       `;
     }
-
     const result = await request.query(query);
-
     res.json(result.recordset);
-
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("Error");
+    } catch (err) {
+     console.error(err);
+     res.status(500).send("Error");
   }
 });
-
+app.get("/", (req, res) => {
+  res.send("🚀 Event Search API is running");
+});
 
 // start server
 app.listen(3000, () => {
